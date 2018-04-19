ac5162c1fa6a192cd8e057d1946f7951451ff7bb
chenyuchenluo <38319466+chenyuchenluo@users.noreply.github.com>
2018-04-19 14:38:14 +0800

    Create 12306.py

---
+++ 12306.py
@@ -0,0 +1,28 @@
+
+# -- coding: utf-8 --
+
+# 加载模块
+import function
+
+if __name__ == '__main__':
+	print(format(u' 开始 ',"*^100"))
+
+	# 初始化参数
+	function.init()
+
+	# 登录账户
+	print(format(u' 开始登录账户 ',"_^40"))
+	result = False
+	while not result:
+		result = function.login()
+
+	# # 设置查询参数
+	print(format(u' 设置查询参数 ',"_^40"))
+	function.inputCheckInfo()
+	function.inputTrainInfo()
+	function.inputSeatInfo()
+	function.getTravers()
+
+	# 开始查询余票
+	print(format(u' 开始查询余票 ',"_^40"))
+	function.timerDelay()
